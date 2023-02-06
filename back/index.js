62eceeb43a433575390ef74a6e2be8d7bb79e16a
AyoubIMAMI <youbima17@gmail.com>
2023-02-06 15:32:16 +0100

    working socket on one port (8000)

--- back/index.js
+++ back/index.js
@@ -4,32 +4,15 @@ const http = require('http')
 const fileQuery = require('./queryManagers/front.js')
 const apiQuery = require('./queryManagers/api.js')
 
-// socket
-const express = require('express');
-const app = express();
-const server = http.createServer(app);
-const { Server } = require('socket.io')
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on("connection",socket=>{
-    console.log(socket.id)
-})
-
-server.listen(3000, () => {
-
-    console.log('listening on *:3000');
-});
+const {Server} = require("socket.io");
 
 /* The http module contains a createServer function, which takes one argument, which is the function that
 ** will be called whenever a new request arrives to the server.
  */
-http.createServer(function (request, response) {
+let server = http.createServer(function (request, response) {
     // First, let's check the URL to see if it's a REST request or a file request.
     // We will remove all cases of "../" in the url for security purposes.
+
     let filePath = request.url.split("/").filter(function(elem) {
         return elem !== "..";
     });
@@ -47,5 +30,15 @@ http.createServer(function (request, response) {
         response.statusCode = 400;
         response.end(`Something in your request (${request.url}) is strange...`);
     }
+
 // For the server to be listening to request, it needs a port, which is set thanks to the listen function.
-}).listen(8000);
+});
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('Connected');
+});
+
+
+server.listen(8000);
