f763591f198fa40a0be9c8f58a93546b10753081
MouradKarrakchou <mourad.karrakchou@gmail.com>
2023-02-11 12:52:14 +0100

    updating game management

--- front/games/local/local_script.js
+++ front/games/local/local_script.js
@@ -1,4 +1,4 @@
-import {colorMessage,checkWin,printIllegalMove,startPlay} from "../gameManagement.js"
+import {colorMessage,checkWin,printIllegalMove} from "../gameManagement.js"
 
 let counter = 0;
 
@@ -13,8 +13,51 @@ function init() {
 
 function play(event){
     if (gameOver) return
-    gameOver=!startPlay(event,counter);
+    gameOver=!startPlay(event);
     counter++;
+}
+
+/**
+ * return false if the game is finished and true is the person still plays
+ * @param event
+ * @returns {boolean|void}
+ */
+function startPlay(event) {
+    if (counter === 42) {
+        console.log("Draw!");
+        document.getElementById("message").innerText = "Draw!";
+        document.getElementById("reset-button").style.display = "block";
+        document.getElementById("reset-button").addEventListener("click", resetGame);
+        return false;
+    }
+    let color = 'red';
+    if (counter % 2 === 0) color = 'yellow';
+
+    let id = event.target.id;
+    let tab = id.split(" ");
+    let column = tab[0];
+    let line = 5;
+
+    id = column + " " + line;
+    if (document.getElementById(id).style.backgroundColor !== "")
+        return printIllegalMove();
+
+    while (line >=0 && document.getElementById(id).style.backgroundColor === "") {
+        line--;
+        id = column + " " + line;
+    }
+
+    line++;
+    id = column + " " + line;
+    document.getElementById(id).style.backgroundColor = color;
+    if (checkWin() === true) {
+        console.log(color + " player wins!");
+        document.getElementById("message").innerText = color + " player wins!";
+        document.getElementById("reset-button").style.display = "block";
+        document.getElementById("reset-button").addEventListener("click", resetGame);
+        return false;
+    }
+    return true;
 }
 
 function resetGame() {
