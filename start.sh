20266b50c0833e8be10f0da32dd3a56788caa30b
AyoubIMAMI <youbima17@gmail.com>
2023-02-11 16:58:52 +0100

    start.sh build

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 echo "Starting Docker containers: Node and MongoDB"
 
+docker-compose build
 docker-compose up
 echo "Stopping Docker containers: Node and MongoDB"
 read -p "Press any key to quit ..."
