19451c384ef9bf6616b7d42ffe048048373fc0ad
MouradKarrakchou <mourad.karrakchou@gmail.com>
2023-02-12 15:40:16 +0100

    save Game

--- back/queryManagers/login/login.js
+++ back/queryManagers/login/login.js
@@ -1,4 +1,6 @@
 const {MongoClient} = require("mongodb");
+const mongoDBConnection = require('../mongoDBConnection');
+
 
 function manageRequest(request, response) {
     const MongoClient = require('mongodb').MongoClient;
@@ -13,34 +15,8 @@ function manageRequest(request, response) {
             });
 
             request.on('end', function () {
-                async function createDatabaseAndUser() {
-                    try {
-                        await client.connect();
-                        let currentUser=JSON.parse(body);
-                        console.log('Connected to MongoDB');
-                        const db = client.db("connect4");
-                        //await db.addUser("admin", "admin", {roles: [{role: "readWrite", db: "connect4"}]});
-                        const usersCollection = db.collection("log");
-                        console.log(currentUser);
-                        console.log({username:currentUser.username,password:currentUser.password});
-                        const user = await usersCollection.findOne({
-                            username: { $regex: new RegExp(currentUser.username, 'i') },
-                            password: { $regex: new RegExp(currentUser.password, 'i') },
-                        });
-                        console.log(user);
-                        response.writeHead(200, {'Content-Type': 'application/json'});
-                        response.end(JSON.stringify({
-                            username:user.username,
-                            token:user.token,}));
-                    } catch (err) {
-                        console.error('Failed to create database or user', err);
-                        response.writeHead(400, {'Content-Type': 'application/json'});
-                        response.end(JSON.stringify({ status: 'failure' }));
-                    } finally {
-                        await client.close();
-                    }
-                }
-                createDatabaseAndUser();
+                let currentUser=JSON.parse(body);
+                mongoDBConnection.createDatabaseAndUser(request,response,currentUser,"log");
             });
         }
         else{
