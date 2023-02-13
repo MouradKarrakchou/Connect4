9a9fa910e5c366099cd9da0e79470bcc4ba73688
AyoubIMAMI <youbima17@gmail.com>
2023-02-13 15:19:16 +0100

    start.sh image clearing added

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 echo "Starting Docker containers: Node and MongoDB"
 
+echo "---------------------RM------------------------"
+docker image rm -f ps8-2023-puissance4quarts_nodejs
+echo "--------------------BUILD----------------------"
 docker-compose build
+echo "---------------------UP------------------------"
 docker-compose up
+
 echo "Stopping Docker containers: Node and MongoDB"
 read -p "Press any key to quit ..."
