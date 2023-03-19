2903c246143edd8dcc1d992af88dad28e919322f
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-03-19 18:27:31 +0100

    fixing bot

--- front/loginRegister/loginRegister.js
+++ front/loginRegister/loginRegister.js
@@ -4,7 +4,7 @@ import {findToken, findUsername} from "../games/gameManagement.js"
 //To switch between local and aws, just change the address variable
 const local = "http://localhost:8000";
 const aws = "http://15.236.190.187:8000";
-const address = local;
+const address = aws;
 
 window.addEventListener("load", function () {
     if(findToken() !== "undefined" && findToken()!==undefined && findUsername() !== "undefined") {
