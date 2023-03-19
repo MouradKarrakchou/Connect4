d228ff2a2b5f2adb7ca4b2bdebc77cfedb61d826
Ayoub <youbima17@gmail.com>
2023-03-19 10:31:47 +0100

    multiplayer game state (win, draw, continue) backend side

---
+++ back/queryManagers/game/gameManagement.js
@@ -0,0 +1,125 @@
+let roomBoardMap = new Map();
+
+function createBoard() {
+    return [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+    ];
+}
+
+/*
+const moveToCheck = {
+                roomId: move.roomId,
+                playerToken: move.playerToken,
+                i: move.i,
+                j: move.password,
+            }
+ */
+
+function checkMove(response, moveToCheck) {
+    let board = roomBoardMap.get(moveToCheck.roomId)
+    let column = moveToCheck.j;
+    let line = moveToCheck.i;
+
+    const player = board[column][line];
+    let count = 1;
+    let j = line;
+    while (j > 0 && board[column][j - 1] === player) {
+        j--;
+        count++;
+    }
+    j = line;
+    while (j < board[column].length - 1 && board[column][j + 1] === player) {
+        j++;
+        count++;
+    }
+    if (count >= 4) {
+        return true;
+    }
+
+    // Check horizontal
+    count = 1;
+    let i = column;
+    while (i > 0 && board[i - 1][line] === player) {
+        i--;
+        count++;
+    }
+    i = column;
+    while (i < board.length - 1 && board[i + 1][line] === player) {
+        i++;
+        count++;
+    }
+    if (count >= 4) {
+        return true;
+    }
+
+    // Check diagonal (top-left to bottom-right)
+
+    count = 1;
+    i = column;
+    j = line;
+    while (i > 0 && j > 0 && board[i - 1][j - 1] === player) {
+        i--;
+        j--;
+        count++;
+    }
+    i = column;
+    j = line;
+    while (i < board.length - 1 && j < board[column].length - 1 && board[i + 1][j + 1] === player) {
+        i++;
+        j++;
+        count++;
+    }
+    if (count >= 4) {
+        return true;
+    }
+
+    // Check diagonal (bottom-left to top-right)
+    count = 1;
+    i = column;
+    j = line;
+    while (i > 0 && j < board[column].length - 1 && board[i - 1][j + 1] === player) {
+        i--;
+        j++;
+        count++;
+    }
+    i = column;
+    j = line;
+    while (i < board.length - 1 && j > 0 && board[i + 1][j - 1] === player) {
+        i++;
+        j--;
+        count++;
+    }
+
+    if (count >= 4) return {
+        state: "over",
+        winner: moveToCheck.playerToken
+    };
+
+    else if (checkDraw(board)) return {
+        state: "over",
+        winner: 0
+    }
+
+    return {
+        state: "continue",
+        winner: "None"
+    }
+}
+
+function checkDraw(board) {
+    // Returns true if the board is full and there is no winner, false otherwise.
+    for (let col = 0; col < 7; col++) {
+        if (board[col][5] === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+exports.manage = manageRequest;
