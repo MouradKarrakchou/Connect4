b183a2168c81dedd85a6f24cfcefe54d3284b335
Weel06 <60885609+weelBenAissa@users.noreply.github.com>
2023-03-26 16:29:53 +0200

    add button profil + start of stats

---
+++ front/profil/profil.js
@@ -0,0 +1,12 @@
+var username = "Nom d'utilisateur";
+var partiesJouees = 50;
+var victoires = 20;
+var defaites = 25;
+var nuls = 5;
+
+// Met à jour les éléments HTML avec les statistiques réelles de l'utilisateur
+document.getElementById("username").innerHTML = username;
+document.getElementById("parties-jouees").innerHTML = partiesJouees;
+document.getElementById("victoires").innerHTML = victoires;
+document.getElementById("defaites").innerHTML = defaites;
+document.getElementById("nuls").innerHTML = nuls;
