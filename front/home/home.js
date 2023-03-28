bda2edbf3d75c59d77f488f3ddc970fe28c55352
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-03-28 12:48:39 +0200

    Fixing all the problems with matchmaking

--- front/home/home.js
+++ front/home/home.js
@@ -14,14 +14,21 @@ socket.on('inQueue', (roomName) => {
     console.log("inQueue");
     document.getElementById("search").style.display = "block";
 });
+socket.on('cancel', () => {
+    document.getElementById("search").style.display = "none";
+});
 let gameSaved=document.getElementById("gameSaved");
+
 
 window.addEventListener('load', function () {
     getAllGames();
     document.getElementById("b").addEventListener('click', findGame);
-    document.getElementById("search").style.display = "none";
+    document.getElementById("cancel").addEventListener('click', cancelGame);
     }
 )
+function cancelGame() {
+    socket.emit('cancelQueue',JSON.stringify({token:token}));
+}
 
 document.getElementById("logout").addEventListener('click', logout);
 
