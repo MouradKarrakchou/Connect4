d3f5b9ec3c933629572c534ffd9bad1432558125
Weel06 <60885609+weelBenAissa@users.noreply.github.com>
2023-04-02 23:32:12 +0200

    aws

--- front/games/dataManager.js
+++ front/games/dataManager.js
@@ -11,7 +11,7 @@
 
 //To switch between local and aws, just change the address variable
 const local = "http://localhost:8000";
-const aws = "http://4quarts.connect4.academy:8000";
+const aws = "http://4quarts.connect4.academy:80";
 export const address = aws;
 export var token;
 
