f702824dd8f51a313dc83a3f32b1745860a630c1
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-04-02 20:00:57 +0200

    notlogged update

--- front/games/dataManager.js
+++ front/games/dataManager.js
@@ -2,7 +2,7 @@
 //To switch between local and aws, just change the address variable
 const local = "http://localhost:8000";
 const aws = "http://4quarts.connect4.academy:8000";
-export const address = aws;
+export const address = local;
 export var token;
 
 export function findToken(){
