40631a0be2d9815507184a20ca429a3148668bd3
Ayoub <youbima17@gmail.com>
2023-04-02 17:41:27 +0200

    loginRegister documentation

--- front/loginRegister/loginRegister.js
+++ front/loginRegister/loginRegister.js
@@ -2,6 +2,20 @@ console.log(document.cookie)
 import {findTokenReturned, findUsername} from "../games/gameManagement.js"
 import {address} from "../games/dataManager.js";
 
+/**
+ * This class manage the login and the registration
+ *
+ * If a user come back here without getting logged out,
+ * he can retrieve his session without connecting again.
+ *
+ * There are securities on the inputs and on the password
+ *
+ * @author Weel BEN AISSA
+ * @author Mourad KARRAKCHOU
+ * @author Ayoub IMAMI
+ */
+
+// Retrieve session if not previously logged out
 window.addEventListener("load", function () {
     localStorage.setItem("theChallengerList", JSON.stringify([]));
     if(findTokenReturned() !== "undefined" && findTokenReturned()!==undefined && findUsername() !== "undefined") {
@@ -13,12 +27,14 @@ window.addEventListener("load", function () {
     }
 })
 
+// Login and Register tabs
 let buttonLog=document.getElementById("login")
 buttonLog.addEventListener("click",login_page)
 
 let buttonReg=document.getElementById("signIn")
 buttonReg.addEventListener("click",signIn_page)
 
+// Show the registration tab
 function signIn_page(){
     buttonLog.style.backgroundColor="#fffbe3d1";
     buttonReg.style.backgroundColor="rgba(150,144,133,0.53)";
@@ -26,6 +42,7 @@ function signIn_page(){
     document.getElementById("reg").style.display="block";
 }
 
+// Show the login tab
 function login_page(){
     buttonReg.style.backgroundColor="#fffbe3d1";
     buttonLog.style.backgroundColor="rgba(150,144,133,0.53)";
@@ -33,6 +50,7 @@ function login_page(){
     document.getElementById("log").style.display="block";
 }
 
+// Login using the button or pressing the 'Enter' key
 document.getElementById("loginButton").addEventListener('click', login);
 document.getElementById("usernameLoginInput").addEventListener("keydown", async function (event) {
     if (event.key === "Enter") {
@@ -46,11 +64,17 @@ document.getElementById("passwordLoginInput").addEventListener("keydown", async 
         await login();
     }
 });
+
+/**
+ * Login: send the inputs to the backend for it to check if the credentials correspond
+ * @returns {Promise<void>}
+ */
 async function login() {
 
     const user = document.getElementsByName("log_name")[0].value;
     const password = hash(document.getElementsByName("log_pswd")[0].value);
 
+    // Security to avoid blank inputs
     if (user === "" || password === "") {
         document.getElementById("errorMessageLogin").innerText = "Please complete all fields";
         document.getElementById("errorMessageLogin").style.display = "block";
@@ -86,6 +110,7 @@ async function login() {
         });
 }
 
+// Register using the button or pressing the 'Enter' key
 document.getElementById("registerButton").addEventListener('click', register);
 document.getElementById("usernameRegisterInput").addEventListener("keydown", async function (event) {
     if (event.key === "Enter") {
@@ -110,24 +135,34 @@ document.getElementById("confirmPasswordRegisterInput").addEventListener("keydow
         await register();
     }
 });
+
+
+/**
+ * Register: send the inputs to the backend for it to check if the username is available
+ * and save the data in the database
+ * @returns {Promise<void>}
+ */
 async function register() {
     const name = document.getElementsByName("reg_name")[0].value;
     const mail = document.getElementsByName("reg_email")[0].value;
     const clearPassword = document.getElementsByName("reg_pswd")[0].value;
     const confirmClearPassword = document.getElementsByName("reg_pswd2")[0].value;
 
+    // Security to avoid blank inputs
     if (name === "" || mail === "" || clearPassword === "" || confirmClearPassword === "") {
         document.getElementById("errorMessage").innerText = "Please complete all fields";
         document.getElementById("errorMessage").style.display = "block";
         return;
     }
 
+    // Security on the email format
     if (!isEmailFormatValid(mail)) {
         document.getElementById("errorMessage").innerText = "Invalid email format - Please enter your email";
         document.getElementById("errorMessage").style.display = "block";
         return;
     }
 
+    // Security on the password validation before sending the data to the backend
     if (confirmPassword(clearPassword, confirmClearPassword)) {
         console.log("passwords are the same");
         const values = {
@@ -152,7 +187,7 @@ async function register() {
                     document.getElementById("errorMessage").style.display = "none";
                     window.alert("Registration successful - You can now log in");
                 }
-            })
+            });
     }
 
     else {
@@ -161,17 +196,21 @@ async function register() {
     }
 }
 
-function hash(str) {
+/**
+ * Caesar code to encrypt the password in the frontend
+ * which will be encrypted again in the backend with an SHA-256 function
+ * @param password
+ * @returns {string} the encrypted password
+ */
+function hash(password) {
     let key = 7;
     let result = "";
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
+    for (let i = 0; i < password.length; i++) {
+        let char = password[i];
         // If the letter is an upper case
         if (char.match(/[A-Z]/i)) {
-            // Getting the ASCII code
-            let code = str.charCodeAt(i);
-            // Add the key to the ASCII code
-            code += key;
+            let code = password.charCodeAt(i); // Getting the ASCII code
+            code += key; // Add the key to the ASCII code
             // Coming back to 'A' if we go over 'Z' (upper case)
             if (char === char.toUpperCase() && code > 90) {
                 code = 64 + (code - 90);
@@ -180,22 +219,31 @@ function hash(str) {
             else if (char === char.toLowerCase() && code > 122) {
                 code = 96 + (code - 122);
             }
-            // Adding to the encrypted string
-            result += String.fromCharCode(code);
+            result += String.fromCharCode(code); // Adding to the encrypted string
         } else {
-            // If the character is not a letter, we let it as it is
-            result += char;
+            result += char; // If the character is not a letter, we let it as it is
         }
     }
     return result;
 }
 
+/**
+ * Verification on the password - are both password inputs are the same
+ * @param clearPassword
+ * @param confirmClearPassword
+ * @returns {boolean} true if they are the same, false otherwise
+ */
 function confirmPassword(clearPassword, confirmClearPassword) {
     const hashedPassword = hash(clearPassword);
     const confirmedHashedPassword = hash(confirmClearPassword);
     return (hashedPassword === confirmedHashedPassword);
 }
 
+/**
+ * Verification on the email format: theUserName@something.somethingElse
+ * @param mail
+ * @returns {boolean} true the mail format is valid
+ */
 function isEmailFormatValid(mail) {
     const emailRegex = /\S+@\S+\.\S+/;
     return emailRegex.test(mail);
