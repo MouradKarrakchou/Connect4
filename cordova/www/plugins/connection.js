e2251a563f8dcb9526d0799e6258f44088f2a77a
Ayoub <youbima17@gmail.com>
2023-04-27 13:51:17 +0200

    connection network plugin

--- cordova/www/plugins/connection.js
+++ cordova/www/plugins/connection.js
@@ -1,22 +1,34 @@
+let states = {};
+states["UNKNOWN"]  = 'Unknown connection';
+states["ETHERNET"] = 'Ethernet connection';
+states["WIFI"]     = 'WiFi connection';
+states["CELL_2G"]  = 'Cell 2G connection';
+states["CELL_3G"]  = 'Cell 3G connection';
+states["CELL_4G"]  = 'Cell 4G connection';
+states["CELL"]     = 'Cell generic connection';
+states["NONE"]     = 'No network connection';
 
 document.addEventListener("offline", onOffline, false);
 
-function onOffline() {
+document.addEventListener("online", onOnline, false);
 
+let overlay;
+
+function onOffline() {
+    console.log(navigator.connection.type);
+    // Create the overlay
+    overlay = document.createElement('div');
+    overlay.id = 'overlay';
+    document.body.appendChild(overlay);
+}
+
+function onOnline() {
+    // Remove the overlay
+    document.body.removeChild(overlay);
 }
 
 function checkConnection() {
     let networkState = navigator.connection.type;
-
-    let states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    alert('Connection type: ' + states[networkState]);
+    if (states[networkState])let a;
+    //alert('Connection type: ' + states[networkState]);
 }
